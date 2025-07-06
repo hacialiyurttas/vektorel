d2864a7b34822862684b3a7ffdfe5194a076411e
hacialiyurttas <h.aliyurttas@gmail.com>
2025-07-06 20:50:40 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,14 @@
+print ("Merhaba")
+print ("Hacı Ali YURTTAŞ")
+print ("Hacı Ali YURTTAŞ\n 999\n 05067184576")
+print ("Korkma, sönmez bu şafaklarda yüzen al sancak;\n Sönmeden yurdumun üstünde tüten en son ocak.\nO benim milletimin yıldızıdır, parlayacak;\nO benimdir, o benim milletimindir ancak.")
+baslik = ("*"*7)+" Öğrenci Bilgileri "+("*"*7)
+print (baslik);
+print ("\tAdı\t\t: Hacı Ali")
+print ("\tSoyadı\t\t: YURTTAŞ")
+print ("\tNumarası\t: 999")
+print ("*"*(len(baslik)))
+print ("Ali'nin annesi \"gel\" dedi")
+print (100*"Hacı Ali YURTTAŞ ")
+print (10*"Hacı Ali YURTTAŞ \n")
+
